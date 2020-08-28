22eb7bb6d40eb40b57cbf1393f6a03dce3c35e04
mono <monobianco87@gmail.com>
2020-08-28 21:04:11 +0200

    Revert "Remove project pathmap"
    This reverts commit 664deed05ad05778bedfc23e2ff6f2e42b7f936c.

--- Android.mk
+++ Android.mk
@@ -1,4 +1,4 @@
-#ifeq ($(call my-dir),$(call project-path-for,qcom-media))
+ifeq ($(call my-dir),$(call project-path-for,qcom-media))
 
 QCOM_MEDIA_ROOT := $(call my-dir)
 $(warning target list is : $(MSM_VIDC_TARGET_LIST))
@@ -15,4 +15,4 @@ include $(QCOM_MEDIA_ROOT)/mm-video-v4l2/Android.mk
 include $(QCOM_MEDIA_ROOT)/libc2dcolorconvert/Android.mk
 endif
 
-#endif
+endif
